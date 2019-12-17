aed849e7a67aa459cabc7153238457a611919b5e
nwscala <nwscala@gmail.com>
2019-12-17 14:17:03 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe 
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand_name)
+        @brand = brand_name
+    end
+
+    def brand 
+        @brand 
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new" 
+    end
+end
